6936df5bab6340072a5f8f033dd4b5540eac39d8
Stephen <stephenbrady365@gmail.com>
2021-07-15 14:45:06 +0100

    Create base service interface. Closes #2.

--- pet-clinic-data/src/main/java/sb/petclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/sb/petclinic/services/PetService.java
@@ -2,13 +2,6 @@ package sb.petclinic.services;
 
 import sb.petclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
+public interface PetService extends CrudService<Pet, Long> {
 }
