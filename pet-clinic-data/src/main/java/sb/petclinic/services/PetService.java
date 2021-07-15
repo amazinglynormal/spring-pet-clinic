72f3109ca710565612c922f0d20140112ae50553
Stephen <stephenbrady365@gmail.com>
2021-07-15 11:34:14 +0100

    Add basic services interfaces

---
+++ pet-clinic-data/src/main/java/sb/petclinic/services/PetService.java
@@ -0,0 +1,14 @@
+package sb.petclinic.services;
+
+import sb.petclinic.model.Pet;
+
+import java.util.Set;
+
+public interface PetService {
+
+    Pet findById(Long id);
+
+    Pet save(Pet pet);
+
+    Set<Pet> findAll();
+}
---
+++ pet-clinic-data/src/main/java/sb/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package sb.petclinic.services;
+
+import sb.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
