7812b1d64506324dea4483808cf5261b60823109
Stephen <stephenbrady365@gmail.com>
2021-08-12 15:50:24 +0100

    Fix broken links

--- pet-clinic-web/src/main/java/sb/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/sb/petclinic/controllers/VetController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import sb.petclinic.services.VetService;
 
 @Controller
+@RequestMapping("/vets")
 public class VetController {
 
     private final VetService vetService;
@@ -16,7 +17,7 @@ public class VetController {
         this.vetService = vetService;
     }
 
-    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    @RequestMapping({"/", "/index", "/index.html"})
     public String listVets(Model model) {
 
         model.addAttribute("vets", vetService.findAll());
