df80decaf01c49b122c9a3ead6328950885723cd
Stephen <stephenbrady365@gmail.com>
2021-08-12 10:42:37 +0100

    Add Pets and Contact info to owners on startup

--- pet-clinic-data/src/main/java/sb/petclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/sb/petclinic/services/map/OwnerServiceMap.java
@@ -2,12 +2,23 @@ package sb.petclinic.services.map;
 
 import org.springframework.stereotype.Service;
 import sb.petclinic.model.Owner;
+import sb.petclinic.model.Pet;
 import sb.petclinic.services.OwnerService;
+import sb.petclinic.services.PetService;
+import sb.petclinic.services.PetTypeService;
 
 import java.util.Set;
 
 @Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
+
+    private final PetTypeService petTypeService;
+    private  final PetService petService;
+
+    public OwnerServiceMap(PetTypeService petTypeService, PetService petService) {
+        this.petTypeService = petTypeService;
+        this.petService = petService;
+    }
 
     @Override
     public Set<Owner> findAll() {
@@ -26,7 +37,30 @@ public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements 
 
     @Override
     public Owner save(Owner object) {
-        return super.save(object);
+
+        Owner savedOwner = null;
+
+        if (object != null) {
+            if (object.getPets() != null) {
+                object.getPets().forEach(pet -> {
+                    if (pet.getPetType() != null) {
+                        if (pet.getPetType().getId() == null) {
+                            pet.setPetType(petTypeService.save(pet.getPetType()));
+                        }
+                    } else {
+                        throw new RuntimeException("Pet Type is required");
+                    }
+
+                    if (pet.getId() == null) {
+                        Pet savedPet = petService.save(pet);
+                        pet.setId(savedPet.getId());
+                    }
+                });
+            }
+            return super.save(object);
+        }
+
+        return null;
     }
 
     @Override
