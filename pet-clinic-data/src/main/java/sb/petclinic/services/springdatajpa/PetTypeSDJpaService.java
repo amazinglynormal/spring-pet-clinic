dedb21035665cfd2fb6be6cb1ac7a9863526c6c0
Stephen <stephenbrady365@gmail.com>
2021-08-13 16:48:01 +0100

    Add Spring Data JPA service for petTypes profile

--- pet-clinic-data/src/main/java/sb/petclinic/services/springdatajpa/PetTypeSDJpaService.java
+++ pet-clinic-data/src/main/java/sb/petclinic/services/springdatajpa/PetTypeSDJpaService.java
@@ -11,7 +11,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Service
-@Profile("Springdatajpa")
+@Profile("springdatajpa")
 public class PetTypeSDJpaService implements PetTypeService {
 
     private final PetTypeRepository petTypeRepository;
